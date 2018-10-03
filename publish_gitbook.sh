4e92d6e675472c649155b2e33ea0c85451523904
김명석 <rrest@icloud.com>
2018-10-03 14:06:26 +0900

    Update docs

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -10,9 +10,8 @@ git pull origin gh-pages --rebase
 # gitbook build로 생긴 _book폴더 아래 모든 정보를 현재 위치로 가져온다.
 cp -R _book/* .
 
-# node_modules폴더와 _book폴더를 지워준다.
 # git clean -fx node_modules
-# git clean -fx _book
+git clean -fx _book
 
 # NOQA
 git add .
